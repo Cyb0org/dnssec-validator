6bd3cbf54cd2a93307a30252c745a6f952cbe60e
Martin Straka <martin.straka@nic.cz>
2012-12-12 09:30:29 +0100

    Update: Link to homepage

--- ie2/DNSSECToolbar/KBBar_p.c
+++ ie2/DNSSECToolbar/KBBar_p.c
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Mon Dec 10 16:01:14 2012
+/* at Wed Dec 12 09:27:41 2012
  */
 /* Compiler settings for KBBar.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
