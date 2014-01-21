014349ee6496c78e8e9b088b05f595af44d072c6
Karel Slany <karel.slany@nic.cz>
2014-01-21 14:55:26 +0100

    TLSA plugin: Added include directory containing common.h.

--- plugin-source/TLSAValidatorPlugin/Mac/projectDef.cmake
+++ plugin-source/TLSAValidatorPlugin/Mac/projectDef.cmake
@@ -36,7 +36,8 @@ add_mac_plugin(${PROJECT_NAME} ${PLIST} ${STRINGS} ${LOCALIZED} SOURCES)
 # set header file directories
 include_directories(${CMAKE_CURRENT_SOURCE_DIR}/../../../libs/openssl/include
                     ${CMAKE_CURRENT_SOURCE_DIR}/../../../libs/ldns/include
-                    ${CMAKE_CURRENT_SOURCE_DIR}/../../../libs/unbound/include)
+                    ${CMAKE_CURRENT_SOURCE_DIR}/../../../libs/unbound/include
+                    ${CMAKE_CURRENT_SOURCE_DIR}/../../../plugin-source/common)
 
 # set static library paths
 add_library(unbound STATIC IMPORTED)
