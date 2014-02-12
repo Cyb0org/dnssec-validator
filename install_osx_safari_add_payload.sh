eaf5f45830827bf637b5dd772ebb038fbffeb5e3
Karel Slany <karel.slany@nic.cz>
2014-02-12 16:04:53 +0100

    AS: Created installation scripts for Safari on OS X.

---
+++ install_osx_safari_add_payload.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env sh
+
+TAR_FILE=arch_$$.tar
+TARGZ_FILE=${TAR_FILE}.gz
+
+SCRIPT_STUB=install_osx_safari_stub.sh
+TARGET=install_osx_safari.sh
+
+PLUGIN_SRC_DIR=plugins-lib
+ADDON_SRC_DIR=add-on
+
+DNSSEC_DIR=npDNSSECValidatorPlugin.plugin
+TLSA_DIR=npTLSAValidatorPlugin.plugin
+SAFARIEXT=safari2.safariextz
+
+function cleanup() {
+	rm -f ${TAR_FILE} ${TARGZ_FILE} ${TARGET}
+}
+
+# Preparation phase.
+cleanup
+
+# Create archive containing plugin stuff.
+if [ ! -d "${PLUGIN_SRC_DIR}/${DNSSEC_DIR}" ]; then
+	echo "Directory ${PLUGIN_SRC_DIR}/${DNSSEC_DIR} does not exist." >&2
+	cleanup
+	exit 1
+fi
+cd "${PLUGIN_SRC_DIR}/"; tar -cf "../${TAR_FILE}" "./${DNSSEC_DIR}" ; cd ..
+if [ ! -d "${PLUGIN_SRC_DIR}/${TLSA_DIR}" ]; then
+	echo "Directory ${PLUGIN_SRC_DIR}/${TLSA_DIR} does not exist." >&2
+	cleanup
+	exit 1
+fi
+cd "${PLUGIN_SRC_DIR}/"; tar -cf "../${TAR_FILE}" "./${TLSA_DIR}" ; cd ..
+if [ ! -f "${ADDON_SRC_DIR}/${SAFARIEXT}" ]; then
+	echo "File ${ADDON_SRC_DIR}/${SAFARIEXT} does not exist." >&2
+	cleanup
+	exit 1
+fi
+cd "${ADDON_SRC_DIR}"; tar -rf "../${TAR_FILE}" "./${SAFARIEXT}" ; cd ..
+gzip "${TAR_FILE}"
+
+cp "${SCRIPT_STUB}" "${TARGET}"
+echo "PAYLOAD:" >> "${TARGET}"
+cat "${TARGZ_FILE}" >> "${TARGET}"
+rm "${TARGZ_FILE}"
+
+chmod +x "${TARGET}"
---
+++ install_osx_safari_stub.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env sh
+
+PLUGIN_DIR="${HOME}/Library/Internet Plug-Ins"
+
+DNSSEC_DIR=npDNSSECValidatorPlugin.plugin
+TLSA_DIR=npTLSAValidatorPlugin.plugin
+SAFARIEXT=safari2.safariextz
+
+uuencode=0
+binary=1
+
+function untar_payload()
+{
+	match=$(grep --text --line-number '^PAYLOAD:$' $0 | cut -d ':' -f 1)
+	payload_start=$((match + 1))
+	if [[ $binary -ne 0 ]]; then
+		tail -n +$payload_start $0 | tar -xzf -
+	fi
+	if [[ $uuencode -ne 0 ]]; then
+		tail -n +$payload_start $0 | uudecode | tar -xzf -
+	fi
+}
+
+#read -p "Install files? " ans
+#if [[ "${ans:0:1}"  ||  "${ans:0:1}" ]]; then
+	untar_payload
+
+	# Do remainder of install steps.
+	if [ -e "${PLUGIN_DIR}/${DNSSEC_DIR}" ]; then
+		echo "Deleting old ${PLUGIN_DIR}/${DNSSEC_DIR}" >&2
+		rm -r "${PLUGIN_DIR}/${DNSSEC_DIR}"
+	fi
+	echo "Instaling ${PLUGIN_DIR}/${DNSSEC_DIR}"
+	mv ${DNSSEC_DIR} "${PLUGIN_DIR}/${DNSSEC_DIR}"
+	if [ -e "${PLUGIN_DIR}/${TLSA_DIR}" ]; then
+		echo "Deleting old ${PLUGIN_DIR}/${TLSA_DIR}" >&2
+		rm -r "${PLUGIN_DIR}/${TLSA_DIR}"
+	fi
+	echo "Installing ${PLUGIN_DIR}/${TLSA_DIR}"
+	mv ${TLSA_DIR} "${PLUGIN_DIR}/${TLSA_DIR}"
+
+	echo ""
+	echo "Now install ${SAFARIEXT} to complete the installation process."
+#fi
+
+exit 0
