2c451ec724576162caecfc1f3dd6461372cb5a7a
Karel Slany <karel.slany@nic.cz>
2014-03-10 12:34:49 +0100

    AS plugin: Minor changes in installer builder script.

--- install_osx_safari_add_payload.sh
+++ install_osx_safari_add_payload.sh
@@ -3,8 +3,16 @@
 TAR_FILE=arch_$$.tar
 TARGZ_FILE=${TAR_FILE}.gz
 
+VERSION_FILE="Version"
+if [ -f ${VERSION_FILE} ]; then
+	VERSION=`cat ${VERSION_FILE}`
+else
+	VERSION="x.y.z"
+fi
+
+PKGS_DIR=packages
 SCRIPT_STUB=install_osx_safari_stub.sh
-TARGET=install_osx_safari.sh
+TARGET_FILE="AS_dnssec-tlsa_validator-${VERSION}-macosx.sh"
 
 PLUGIN_SRC_DIR=plugins-lib
 ADDON_SRC_DIR=add-on
@@ -14,13 +22,18 @@ TLSA_DIR=npTLSAValidatorPlugin.plugin
 SAFARIEXT=safari2.safariextz
 
 function cleanup() {
-	rm -f ${TAR_FILE} ${TARGZ_FILE} ${TARGET}
+	rm -f "${TAR_FILE}" "${TARGZ_FILE}" "${PKGS_DIR}/${TARGET_FILE}"
 }
+
+# Check whether target directory exists.
+if [ ! -d "${PKGS_DIR}" ]; then
+	mkdir ${PKGS_DIR} || exit 1
+fi
 
 # Preparation phase.
 cleanup
 
-# Create archive containing plugin stuff.
+# Create archive containing plug-in stuff.
 if [ ! -d "${PLUGIN_SRC_DIR}/${DNSSEC_DIR}" ]; then
 	echo "Directory ${PLUGIN_SRC_DIR}/${DNSSEC_DIR} does not exist." >&2
 	cleanup
@@ -41,9 +54,9 @@ fi
 cd "${ADDON_SRC_DIR}"; tar -rf "../${TAR_FILE}" "./${SAFARIEXT}" ; cd ..
 gzip "${TAR_FILE}"
 
-cp "${SCRIPT_STUB}" "${TARGET}"
-echo "PAYLOAD:" >> "${TARGET}"
-cat "${TARGZ_FILE}" >> "${TARGET}"
+cp "${SCRIPT_STUB}" "${PKGS_DIR}/${TARGET_FILE}"
+echo "PAYLOAD:" >> "${PKGS_DIR}/${TARGET_FILE}"
+cat "${TARGZ_FILE}" >> "${PKGS_DIR}/${TARGET_FILE}"
 rm "${TARGZ_FILE}"
 
-chmod +x "${TARGET}"
+chmod +x "${PKGS_DIR}/${TARGET_FILE}"
