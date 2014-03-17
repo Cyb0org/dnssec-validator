540f277bb2ab1a4c515b13d23a48bc7338ad5a67
Karel Slany <karel.slany@nic.cz>
2014-03-17 17:28:03 +0100

    Plugins: Enhanced ppapi compilation scripts.

--- compile_ppapi.sh
+++ compile_ppapi.sh
@@ -1,26 +1,47 @@
 #!/usr/bin/env sh
 
 export NACL_SDK_ROOT=${HOME}/nacl_sdk/pepper_33
-NACL_SDK_DIR=${NACL_SDK_ROOT}/toolchain/linux_x86_glibc/bin
-
 if [ ! -d "${NACL_SDK_ROOT}" ]; then
 	echo "Cannot find ${NACL_SDK_ROOT}" >&2
 	exit 1
 fi
 
-if [ ! -d "${NACL_SDK_DIR}" ]; then
-	echo "Cannot find ${NACL_SDK_DIR}" >&2
+export OSNAME=`${NACL_SDK_ROOT}/tools/getos.py`
+export X86_TC_PATH=${NACL_SDK_ROOT}/toolchain/${OSNAME}_x86_glibc
+export ARM_TC_PATH=${NACL_SDK_ROOT}/toolchain/${OSNAME}_arm_newlib
+
+if [ ! -d "${X86_TC_PATH}" ]; then
+	echo "Cannot find ${X86_TC_PATH}" >&2
 	exit 1
 fi
 
-export AR=i686-nacl-ar
-export CC=i686-nacl-gcc
-export LD=i686-nacl-ld
-export RANLIB=i686-nacl-ranlib
-export HOST=i686-nacl
-export MACHINE=i686
+export PATH=${X86_TC_PATH}/bin:${ARM_TC_PATH}/bin:${PATH}
 
-export PATH=${NACL_SDK_DIR}:${PATH}
+export CPPFLAGS="-I${NACL_SDK_ROOT}/include"
+
+DEBUG_PPAPI="no"
+
+if [ "x${DEBUG_PPAPI}" = "xyes" ]; then
+	# Debugging version of the toolkit.
+	export LDFLAGS="-L${NACL_SDK_ROOT}/lib/glibc_x86_32/Debug ${LDFLAGS}"
+else
+	# Release version of the toolkit.
+	export LDFLAGS="-L${NACL_SDK_ROOT}/lib/glibc_x86_32/Release ${LDFLAGS}"
+fi
+
+#
+# List of supported hosts.
+#
+HOSTS=""
+#MACHINES="i686 x86_64 arm"
+MACHINES="i686 x86_64"
+#MACHINES="i686"
+#MACHINES="x86_64"
+#MACHINES="arm"
+for MACHINE in ${MACHINES}; do
+	HOSTS="${HOSTS} ${MACHINE}-nacl"
+done
+
 
 SCRIPT_LOCATION=$(dirname $(readlink -f $0))
 
@@ -28,11 +49,19 @@ OPENSSL_DIR=libs/openssl-1.0.1f
 LDNS_DIR=libs/ldns-1.6.17
 UNBOUND_DIR=libs/unbound-1.4.22
 
-PREFIX=${SCRIPT_LOCATION}/ppapi_built
+BUILT_DIR=${SCRIPT_LOCATION}/ppapi_built
 
-if [ ! -d "${PREFIX}" ]; then
-	mkdir ${PREFIX}
+if [ ! -d "${BUILT_DIR}" ]; then
+	mkdir ${BUILT_DIR}
 fi
+
+for HOST in ${HOSTS}; do
+	PREFIX="${BUILT_DIR}/${OSNAME}-${HOST}"
+	echo ${PREFIX}
+	if [ ! -d "${PREFIX}" ]; then
+		mkdir ${PREFIX}
+	fi
+done
 
 COMPILE_SSL="yes"
 COMPILE_LDNS="yes"
@@ -40,41 +69,90 @@ COMPILE_UNBOUND="yes"
 COMPILE_DNSSEC="yes"
 
 if [ "x${COMPILE_SSL}" = "xyes" ]; then
-	#NEWLIB_OPTS="no-dso no-sock no-ui"
-	cd ${OPENSSL_DIR}
-	make clean
-	CMD="./config no-shared no-asm no-hw no-krb5 -D_GNU_SOURCE --prefix=${PREFIX}"
-	${CMD} && make && make install
-	cd ${SCRIPT_LOCATION}
+	for MACHINE in ${MACHINES}; do
+		export MACHINE
+		export HOST=${MACHINE}-nacl
+		export AR=${HOST}-ar
+		export CC=${HOST}-gcc
+		export CXX=${HOST}-g++
+		export LD=${HOST}-ld
+		export RANLIB=${HOST}-ranlib
+
+		PREFIX="${BUILT_DIR}/${OSNAME}-${HOST}"
+
+		#NEWLIB_OPTS="no-dso no-sock no-ui"
+		cd ${OPENSSL_DIR}
+		make clean
+		CMD="./config no-shared no-asm no-hw no-krb5 -D_GNU_SOURCE --prefix=${PREFIX}"
+		${CMD} && make && make install
+		cd ${SCRIPT_LOCATION}
+	done
 fi
 
 #exit
 
 if [ "x${COMPILE_LDNS}" = "xyes" ]; then
-	cd ${LDNS_DIR}
-	make clean
-	CMD="./configure --host=${HOST} --disable-shared --with-ssl=${PREFIX} --prefix=${PREFIX} --disable-ldns-config --with-pic --without-pyldnsx"
-	${CMD} && make && make install
-	cd ${SCRIPT_LOCATION}
+	for MACHINE in ${MACHINES}; do
+		export MACHINE
+		export HOST=${MACHINE}-nacl
+		export AR=${HOST}-ar
+		export CC=${HOST}-gcc
+		export CXX=${HOST}-g++
+		export LD=${HOST}-ld
+		export RANLIB=${HOST}-ranlib
+
+		PREFIX="${BUILT_DIR}/${OSNAME}-${HOST}"
+
+		cd ${LDNS_DIR}
+		make clean
+		CMD="./configure --host=${HOST} --disable-shared --with-ssl=${PREFIX} --prefix=${PREFIX} --disable-ldns-config --with-pic --without-pyldnsx"
+		${CMD} && make && make install
+		cd ${SCRIPT_LOCATION}
+	done
 fi
 
 #exit
 
 if [ "x${COMPILE_UNBOUND}" = "xyes" ]; then
-	cd ${UNBOUND_DIR}
-	make clean
-	CMD="./configure --host=${HOST} --disable-shared --with-ssl=${PREFIX} --with-ldns=${PREFIX} --prefix=${PREFIX} --with-libunbound-only"
-	${CMD} && make && make install
-	cd ${SCRIPT_LOCATION}
+	for MACHINE in ${MACHINES}; do
+		export MACHINE
+		export HOST=${MACHINE}-nacl
+		export AR=${HOST}-ar
+		export CC=${HOST}-gcc
+		export CXX=${HOST}-g++
+		export LD=${HOST}-ld
+		export RANLIB=${HOST}-ranlib
+
+		PREFIX="${BUILT_DIR}/${OSNAME}-${HOST}"
+
+		cd ${UNBOUND_DIR}
+		make clean
+		CMD="./configure --host=${HOST} --disable-shared --with-ssl=${PREFIX} --with-ldns=${PREFIX} --prefix=${PREFIX} --with-libunbound-only"
+		${CMD} && make && make install
+		cd ${SCRIPT_LOCATION}
+	done
 fi
 
 # exit
 
 if [ "x${COMPILE_DNSSEC}" = "xyes" ]; then
-	export CPPFLAGS="-I${PREFIX}/include"
-	export LDFLAGS="-L${PREFIX}/lib"
-	make -f Makefile.ppapi clean
-	make -f Makefile.ppapi
-	make -f Makefile.ppapi install
-	make -f Makefile.ppapi clean
+	for MACHINE in ${MACHINES}; do
+		export MACHINE
+		export HOST=${MACHINE}-nacl
+		export AR=${HOST}-ar
+		export CC=${HOST}-gcc
+		export CXX=${HOST}-g++
+		export LD=${HOST}-ld
+		export RANLIB=${HOST}-ranlib
+
+		PREFIX="${BUILT_DIR}/${OSNAME}-${HOST}"
+
+		export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+		export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+
+		make -f Makefile.ppapi clean
+		make -f Makefile.ppapi
+		make -f Makefile.ppapi install
+		make -f Makefile.ppapi clean
+	done
 fi
