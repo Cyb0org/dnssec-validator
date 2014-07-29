2a5c4f48d1346cbfa73058745b5fe2fd533780d5
Karel Slany <karel.slany@nic.cz>
2014-07-29 14:59:38 +0200

    GC: Script for distributing/packing native messaging extension.

---
+++ install_chrome_add_payload.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env sh
+
+
+# Script must be run after these commands:
+#make -f Makefile.main clean && cmake . && make
+#cd tests && make clean && make && cd ..
+
+
+CHROME_BINARY="google-chrome-stable"
+
+TARGZ_FILE=arch_$$.tar.gz
+
+VERSION_FILE="Version"
+if [ -f ${VERSION_FILE} ]; then
+	VERSION=`cat ${VERSION_FILE}`
+else
+	VERSION="x.y.z"
+fi
+
+if [ "x${HWARCH}" = "x" ]; then
+	HWARCH=unknown
+fi
+
+SYSTEM=`uname -s | tr '[:upper:]' '[:lower:]'`
+
+TARGET_DNSSEC="GC-dnssec_validator-${VERSION}-${SYSTEM}-${HWARCH}.sh"
+TARGET_TLSA="GC-tlsa_validator-${VERSION}-${SYSTEM}-${HWARCH}.sh"
+
+SCRIPT_STUB="install_chrome_stub.sh"
+
+#TEMP_DIR=`mktemp -d`
+rm -rf __tmp__ && mkdir __tmp__
+TEMP_DIR="__tmp__"
+
+function cleanup() {
+	rm -rf "${TEMP_DIR}" "${TARGZ_FILE}" "${TARGET_DNSSEC}" "${TARGET_TLSA}"
+}
+
+
+#cleanup
+cp -r add-on/chrome-dnssec/native-msg_built ${TEMP_DIR}/dnssec-pkg
+mv ${TEMP_DIR}/dnssec-pkg/cz.nic.validator.dnssec.json.in ${TEMP_DIR}/cz.nic.validator.dnssec.json.in
+rm ${TEMP_DIR}/dnssec-pkg/cz.nic.validator.dnssec.json*
+cp tests/dnssec-plug ${TEMP_DIR}/dnssec-plug
+cp ../chrome_dnssec_validator.pem ${TEMP_DIR}/chrome_dnssec_validator.pem
+${CHROME_BINARY} --pack-extension="${TEMP_DIR}/dnssec-pkg" --pack-extension-key="${TEMP_DIR}/chrome_dnssec_validator.pem"
+rm ${TEMP_DIR}/chrome_dnssec_validator.pem
+#
+cd ${TEMP_DIR}; tar -czf "${TARGZ_FILE}" cz.nic.validator.dnssec.json.in dnssec-plug dnssec-pkg.crx; cd ..
+cp "${SCRIPT_STUB}" "${TARGET_DNSSEC}"
+echo "PAYLOAD:" >> "${TARGET_DNSSEC}"
+cat "${TEMP_DIR}/${TARGZ_FILE}" >> "${TARGET_DNSSEC}"
+chmod +x "${TARGET_DNSSEC}"
+rm -f ${TARGZ_FILE}
+
+
+#cleanup
+mkdir ${TEMP_DIR}
+cp -r add-on/chrome-tlsa/native-msg_built ${TEMP_DIR}/tlsa-pkg
+mv ${TEMP_DIR}/tlsa-pkg/cz.nic.validator.tlsa.json.in ${TEMP_DIR}/cz.nic.validator.tlsa.json.in
+rm ${TEMP_DIR}/tlsa-pkg/cz.nic.validator.tlsa.json*
+cp tests/dane-plug ${TEMP_DIR}/dane-plug
+cp ../chrome_tlsa_validator.pem ${TEMP_DIR}/chrome_tlsa_validator.pem
+${CHROME_BINARY} --pack-extension="${TEMP_DIR}/tlsa-pkg" --pack-extension-key="${TEMP_DIR}/chrome_tlsa_validator.pem"
+rm ${TEMP_DIR}/chrome_tlsa_validator.pem
+#
+cd ${TEMP_DIR}; tar -czf "${TARGZ_FILE}" cz.nic.validator.tlsa.json.in dane-plug tlsa-pkg.crx; cd ..
+cp "${SCRIPT_STUB}" "${TARGET_TLSA}"
+echo "PAYLOAD:" >> "${TARGET_TLSA}"
+cat "${TEMP_DIR}/${TARGZ_FILE}" >> "${TARGET_TLSA}"
+rm -f ${TARGZ_FILE}
+chmod +x "${TARGET_TLSA}"
+
+
+rm -rf ${TEMP_DIR}
---
+++ install_chrome_stub.sh
@@ -0,0 +1,97 @@
+#!/usr/bin/env sh
+
+PLUGIN_DIR="${HOME}/Library/Internet Plug-Ins"
+
+DNSSEC_DIR=npDNSSECValidatorPlugin.plugin
+TLSA_DIR=npTLSAValidatorPlugin.plugin
+SAFARIEXT=safari2.safariextz
+
+uuencode=0
+binary=1
+
+function untar_payload()
+{
+	SCRIPT="$0"
+	if [ "x$1" != "x" ]; then
+		SCRIPT="$1"
+	fi
+
+	match=$(grep --text --line-number '^PAYLOAD:$' "${SCRIPT}" | cut -d ':' -f 1)
+	payload_start=$((match + 1))
+	if [[ $binary -ne 0 ]]; then
+		tail -n +$payload_start "${SCRIPT}" | tar -xzf -
+	fi
+	if [[ $uuencode -ne 0 ]]; then
+		tail -n +$payload_start "${SCRIPT}" | uudecode | tar -xzf -
+	fi
+}
+
+USAGE="Usage:\n\t $0 [core_directory]\n"
+
+SCRIPT_NAME=$(basename $0)
+BASEDIR=$(dirname $(readlink -f $0))
+CORE_DIR=$1
+
+#read -p "Install files? " ans
+#if [[ "${ans:0:1}"  ||  "${ans:0:1}" ]]; then
+	# Detect whether binary path entered or whether to use default.
+	if [ "x${CORE_DIR}" = "x" ]; then
+		CORE_DIR="${HOME}"/chrome_native_msg_cores
+	fi
+
+	if [ ! -d "${CORE_DIR}" ]; then
+		mkdir -p ${CORE_DIR}
+	fi
+
+	# Detect where to install JSON file.
+	SYSTEM=`uname -s | tr '[:upper:]' '[:lower:]'`
+	# https://developer.chrome.com/extensions/messaging#native-messaging
+	MANIFEST_DIR=""
+	case "${SYSTEM}" in
+	linux)
+		MANIFEST_DIR="${HOME}/.config/google-chrome/NativeMessagingHosts"
+		;;
+	darwin)
+		MANIFEST_DIR="${HOME}/Library/Application Support/Google/Chrome/NativeMessagingHosts"
+		;;
+	*)
+		echo >&2 "Unsupported system '${SYSTEM}'."
+		exit 1
+		;;
+	esac
+
+	if [ ! -d "${MANIFEST_DIR}" ]; then
+		mkdir -p "${MANIFEST_DIR}"
+	fi
+
+	WORK_DIR=`pwd`
+
+	TMP_DIR=`mktemp -d`
+	cd "${TMP_DIR}"; untar_payload ${BASEDIR}/${SCRIPT_NAME}; cd "${WORK_DIR}"
+
+	PLUG_FILE=`ls "${TMP_DIR}" | grep plug`
+	JSON_IN_FILE=`ls "${TMP_DIR}" | grep json.in`
+	CRX_FILE=`ls "${TMP_DIR}" | grep crx`
+	JSON_FILE=`echo ${JSON_IN_FILE} | sed -e 's/\.in$//g'`
+
+	# Move binary to location.
+	cp "${TMP_DIR}/${PLUG_FILE}" "${CORE_DIR}/${PLUG_FILE}"
+
+	# Update JSON template.
+	ESCAPED_PATH=`echo "${CORE_DIR}/${PLUG_FILE}" | sed -e 's/\//\\\\\//g'`
+	sed -e "s/[@][^_]*_BINARY[@]/\"${ESCAPED_PATH}\"/g" < "${TMP_DIR}/${JSON_IN_FILE}" > "${MANIFEST_DIR}/${JSON_FILE}"
+
+	# Move crx file.
+	cp "${TMP_DIR}/${CRX_FILE}" "${WORK_DIR}/${CRX_FILE}"
+	echo "A CRX file has been created in the current directory."
+	echo "Install the file '${WORK_DIR}/${CRX_FILE}' into Google Chrome using drag add drop:"
+	echo -e "\t1) Open Google Chrome."
+	echo -e "\t2) Open the page chrome://extensions/ ."
+	echo -e "\t3) Drag the CRX file into the page and accept the notification."
+	echo -e "\t4) Restart Google Chrome."
+
+	# Do remainder of install steps.
+	rm -rf "${TMP_DIR}"
+#fi
+
+exit 0
